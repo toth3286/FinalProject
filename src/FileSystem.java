cefebc09bdd51a9df3aff4d4dcbe3822ffc3d0bb
chris <chris@chris-HP-Pavilion-dm4-Notebook-PC>
2013-11-26 14:09:00 -0800

    Added FileSystem

---
+++ src/FileSystem.java
@@ -0,0 +1,39 @@
+
+public class FileSystem {
+
+	public FileSystem(){
+		
+	}
+	
+	public int format(int files){
+		
+	}
+	
+	public int open(String fileName, String mode){
+		
+	}
+	
+	public int read(int fd, byte buffer[]){
+		
+	}
+	
+	public int write(int fd, byte buffer[]){
+		
+	}
+	
+	int seek(int fd, int offset, int whence){
+		
+	}
+	
+	int close(int fd){
+		
+	}
+	
+	int delete(String fileName){
+		
+	}
+	
+	int fsize(int fd){
+		
+	}
+}
